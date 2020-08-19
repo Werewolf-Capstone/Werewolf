a2e4202735a43150f51742d1dfa88c557d72e5e2
Cameron Holsinger <echolsinger@me.com>
2020-08-19 18:13:47 -0400

    added sign-up component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -21,6 +21,9 @@ const Navigation = () => (
       <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
       </li>
+      <li>
+        <Link to={ROUTES.SIGN_UP}>Sign-Up</Link>
+      </li>
     </ul>
   </div>
 );
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,108 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
  
-const SignUp = () => (
+import * as ROUTES from '../../constants/routes';
+ 
+const SignUpPage = () => (
   <div>
     <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
  
-export default SignUp;
+const initialState = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+  };
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...initialState };
+  }
+ 
+  onSubmit = event => {
+    const { username, email, passwordOne } = this.state;
+ 
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({ ...initialState });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+ 
+    event.preventDefault();
+  }
+ 
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+ 
+  render() {
+    const {
+        username,
+        email,
+        passwordOne,
+        passwordTwo,
+        error,
+      } = this.state;
+
+      const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+          <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button disabled={isInvalid} type="submit">Sign Up</button>
+ 
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+ 
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+ 
+export default SignUpPage;
+ 
+export { SignUpForm, SignUpLink };
