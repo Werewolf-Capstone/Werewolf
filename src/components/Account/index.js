a754b53343c6db7daa7bbdd9adb1347112b0bcc6
Jonathan Albert <JonathanAlbert0115@gmail.com>
2020-08-20 12:42:49 -0400

    Added a change password form to the user account page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
- 
-const Account = () => (
+import React from "react";
+
+import PasswordChangeForm from "../PasswordChange";
+
+const AccountPage = () => (
   <div>
-    <h1>Account</h1>
+    <h1>Account Page</h1>
+    <PasswordChangeForm />
   </div>
 );
- 
-export default Account;
+
+export default AccountPage;
