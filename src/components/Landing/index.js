bc267f5f170da7858a7683b9f150809cbad83047
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2020-08-25 21:58:55 -0400

    join room button works

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,63 +1,69 @@
-import React, { useState } from "react";
-import {
-  TextField,
-  Container,
-  Button,
-  Box,
-  FormControl,
-} from "@material-ui/core";
-import { withFirebase } from "../Firebase";
-import { withRouter } from "react-router-dom";
+import React, { useState } from 'react';
+import { TextField, Container, Button, Box } from '@material-ui/core';
+import { withFirebase } from '../Firebase';
+import { withRouter } from 'react-router-dom';
 
-const Landing = (props) => {
-  const [roomId, setRoomId] = useState("");
-
-  async function handleOnNewGame() {
-    const roomId = await props.firebase.createRoom();
-    props.history.push(`/GameRoom/${roomId}`);
+class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomId: '',
+    };
+    this.handleNewGame = this.handleNewGame.bind(this);
+    this.handleJoinGame = this.handleJoinGame.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
   }
 
-  async function handleOnJoinGame(event) {
-    props.history.push(`/GameRoom/${event.value}`);
+  async handleNewGame() {
+    const roomId = await this.props.firebase.createRoom();
+    this.props.history.push(`/GameRoom/${roomId}`);
   }
 
-  return (
-    <div>
-      <h1>Landing</h1>
-      <Container maxWidth="sm">
-        <Box display="flex" flexDirection="column" width="60%">
-          <Box display="flex" flexDirection="row" width="100%">
-            <Button
-              variant="outlined"
-              color="secondary"
-              width="100%"
-              onClick={handleOnNewGame}
-            >
-              New Game
-            </Button>
-          </Box>
-          <Box display="flex" flexDirection="row" width="100%">
-            <FormControl>
-              <TextField
-                variant="outlined"
-                label="Join Game ID"
-                onChange={setRoomId}
-              />
+  handleOnChange = (event) => {
+    this.setState({
+      roomId: event.target.value,
+    });
+  };
+
+  // should we do error checking for rooms that don't exist when entering full URL manually?
+  async handleJoinGame(event) {
+    this.props.history.push(`/GameRoom/${this.state.roomId}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Landing</h1>
+        <Container maxWidth='sm'>
+          <Box display='flex' flexDirection='column' width='60%'>
+            <Box display='flex' flexDirection='row' width='100%'>
               <Button
-                type="submit"
-                variant="outlined"
-                color="secondary"
-                onClick={handleOnJoinGame}
-                // disabled={isInvalid}
+                variant='outlined'
+                color='secondary'
+                width='100%'
+                onClick={this.handleNewGame}
               >
-                Join Game
+                New Game
               </Button>
-            </FormControl>
+            </Box>
+            <Box display='flex' flexDirection='row' width='100%'>
+              <form onSubmit={this.handleJoinGame}>
+                <TextField
+                  variant='outlined'
+                  label='Join Game ID'
+                  value={this.state.roomId}
+                  onChange={this.handleOnChange}
+                />
+                <Button type='submit' variant='outlined' color='secondary'>
+                  Join Game
+                </Button>
+              </form>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </div>
-  );
-};
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default withRouter(withFirebase(Landing));
