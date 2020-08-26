32defff0172351408c38e4ac11094aaa1d5762ad
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2020-08-26 00:15:12 -0400

    some styling

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,49 +1,51 @@
-import React, { useEffect, useState } from "react";
-import * as ROLES from "../../constants/roles";
-import { withFirebase } from "../Firebase";
-// import "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import { withFirebase } from '../Firebase';
+
 const Admin = (props) => {
- 
   const [users, setUsers] = useState(null);
-//   const [loading, setLoading] = useState(true);
- 
+  //   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     // setLoading(true);
-    props.firebase.users()
-    .then((snapshot) => {
+    props.firebase
+      .users()
+      .then((snapshot) => {
         if (!snapshot) {
-            setUsers([])
+          setUsers([]);
         } else {
-            let userList = []
-            snapshot.forEach( user => {
-                // console.log(user.data())
-                userList.push({ key: user.id, ...user.data() })
-            })
-            setUsers(userList)
+          let userList = [];
+          snapshot.forEach((user) => {
+            // console.log(user.data())
+            userList.push({ key: user.id, ...user.data() });
+          });
+          setUsers(userList);
         }
       })
-      .catch(error => {
-          //make an error handler
-      })
-  
+      .catch((error) => {
+        //make an error handler
+      });
   }, []);
 
   let displayList;
-    if (users === null) {
-      displayList = <li>LOADING...</li>
-    } else if (users.length === 0) {
-        displayList = <li>NO USERS FOUND</li>
-    } else {
-        displayList = users.map(user => {
-        return <li key={user.key}><div>Username: {user.username} </div> <div>Email: {user.email}</div></li>
-        })
-    }
+  if (users === null) {
+    displayList = <li>LOADING...</li>;
+  } else if (users.length === 0) {
+    displayList = <li>NO USERS FOUND</li>;
+  } else {
+    displayList = users.map((user) => {
+      return (
+        <li key={user.key}>
+          <div>Username: {user.username} </div> <div>Email: {user.email}</div>
+        </li>
+      );
+    });
+  }
 
   return (
     <div>
       <h1>Admin</h1>
       <p>Restricted area! Only users with the admin role are authorized.</p>
-  <ul>{displayList}</ul>
+      <ul>{displayList}</ul>
     </div>
   );
 };
